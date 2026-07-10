69882acac96dd70142d6a49ed964e64d35b21dbb
spenbernal <158215783+spenbernal@users.noreply.github.com>
2026-07-10 12:13:06 -0400

    batched KNN for predict

--- knn.py
+++ knn.py
@@ -11,13 +11,16 @@ class KNN:
         self.y_train = y_train
     
     def predict(self, X_test):
-        # X_test: D,
-        distances = self.distance(X_test, self.X_train) # N,
-        top_K_idxs = np.argsort(distances)[:self.K]
-        K_labels = self.y_train[top_K_idxs]
-        vals, counts = np.unique(K_labels, return_counts=True)
-        pred = vals[np.argmax(counts)]
-        return pred
+        # X_test: M,D
+        distances = self.distance(X_test, self.X_train) # M, N
+        top_K_idxs = np.argsort(distances, axis= -1)[:, :self.K] # M, K
+        K_labels = self.y_train[top_K_idxs] 
+        preds = []
+        for label in K_labels:
+            vals, counts = np.unique(label, return_counts=True)
+            pred = vals[np.argmax(counts, axis= -1)]
+            preds.append(pred)
+        return np.array(preds)
         
         
         
