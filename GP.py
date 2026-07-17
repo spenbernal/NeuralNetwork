e1bd3155a133297edf7830926620464898dbd16c
spenbernal <158215783+spenbernal@users.noreply.github.com>
2026-07-16 23:11:02 -0400

    added noisy predictions

--- GP.py
+++ GP.py
@@ -1,10 +1,11 @@
 import numpy as np
 class GaussianProcess:
-    def __init__(self, mercer_kernel, mean_func: np.vectorize) -> None:
+    def __init__(self, mercer_kernel, mean_func: np.vectorize, noise_var= 0.0) -> None:
         self.mercer_kernel = mercer_kernel
         self.mean_func = mean_func
+        self.noise_var = noise_var
         
-    def fit(self, X_train, y_train):
+    def fit(self, X_train: np.ndarray, y_train: np.ndarray):
         # X: N, D
         # Y: N,
         self.X_train = X_train
@@ -23,5 +24,22 @@ class GaussianProcess:
         
         preds = np.random.multivariate_normal(mean= mu, cov= Sigma, size= X_test.size)
         return preds
+    
+    # Noisy predictions
+    def predict_(self, X_test):
+        # M, D
+        mu_X = self.mean_func(self.X_train)
+        mu_star = self.mean_func(X_test)
+        K_XX = self.mercer_kernel(self.X_train, self.X_train)
+        K_X_star = self.mercer_kernel(self.X_train, X_test)
+        K_star_star = self.mercer_kernel(X_test, X_test)
+        K_sigma = K_XX + self.noise_var * np.eye(self.y_train.size)
+        mu_posterior = mu_star + K_X_star.T @ np.linalg.solve(K_sigma, self.y_train - mu_X)
+        Sigma_posterior = K_star_star - K_X_star.T @ np.linalg.solve(K_sigma, K_X_star)
+        return mu_posterior, Sigma_posterior
         
+
+class MercerKernels:
+    def rbf(self, bandwidth):
+        return 
         
